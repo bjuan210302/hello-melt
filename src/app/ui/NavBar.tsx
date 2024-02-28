ba8dea1daa0ecfead169c35ed1a95c564f491a40
Juan Bustamante <bjuan210302@gmail.com>
2024-02-28 18:06:02 -0500

    style changes to nav

--- src/app/ui/NavBar.tsx
+++ src/app/ui/NavBar.tsx
@@ -1,31 +1,33 @@
 'use client'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
-export const NavBar = (props: {className?: string}) => {
+export const NavBar = (props: { className?: string }) => {
   const pathname = usePathname()
   return (
     <div
       className={`${props.className} flex absolute gap-8
-      font-normal
-      flex-row rounded-full px-6 bottom-6
+      flex-row rounded-full px-6 bottom-10
       left-6 py-2 shadow-lg
       2xl:left-1/2 2xl:-translate-x-1/2
       drop-shadow-2xl
-      text-white
-      bg-[#FF575A]`}
+      text-[#101828] font-normal
+      border-2 border-indigo-300`}
     >
-      <a
-        href="route1">
-        <span className="tracking-widest text-indigo-200">/</span>route1</a>
-      <a
-        href="route2"
-      ><span className="tracking-widest text-indigo-200">/</span>route2</a>
-      <a
-        href="route3"
-      ><span className="tracking-widest text-indigo-200">/</span>route3</a>
-      <a
-        href="route4"
-      ><span className="tracking-widest text-indigo-200">/</span>route4</a>
+      <Link className={`${pathname === '/' ? 'text-indigo-300' : ''} hover:text-indigo-300 transition-all`} href="/">Home</Link>
+
+      <Link className={`${pathname === '/route2' ? 'text-indigo-300' : ''} hover:text-indigo-300 transition-all`} href="route2">
+        <span className="tracking-widest opacity-40">/</span>
+        route2
+      </Link>
+      <Link className={`${pathname === '/route3' ? 'text-indigo-300' : ''} hover:text-indigo-300 transition-all`} href="route3">
+        <span className="tracking-widest opacity-40">/</span>
+        route3
+      </Link>
+      <Link className={`${pathname === '/route4' ? 'text-indigo-300' : ''} hover:text-indigo-300 transition-all`} href="route4">
+        <span className="tracking-widest opacity-40">/</span>
+        route4
+      </Link>
     </div>
   )
 }
